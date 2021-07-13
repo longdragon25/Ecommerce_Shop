8ded12a5639c0ac16a28deb851deaa8d4fa84352
longdragon25 <longdragon25112000xt@gmail.com>
2021-07-13 10:39:12 +0700

    Navigator

---
+++ Navigators/Main.js
@@ -0,0 +1,66 @@
+import { Text, View } from "react-native";
+
+import AdminNavigator from "./AdminNavigator";
+import CartNavigator from "./CartNavigator";
+import HomeNavigator from "./HomeNavigator";
+import Icon from "react-native-vector-icons/FontAwesome";
+import React from "react";
+import UserNavigator from "./UserNavigator";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+const Main = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        keyboardHidesTabBar: true,
+        showLabel: false,
+        activeTintColor: "#e91e63",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="home" color={color} size={30} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Cart"
+        component={CartNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="shopping-cart" color={color} size={30} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Admin"
+        component={AdminNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="cog" color={color} size={30} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="User"
+        component={UserNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="user" color={color} size={30} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Main;
